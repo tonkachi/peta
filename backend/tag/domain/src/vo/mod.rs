f2d40ce2d6298dde898687464d67ad7faab07b18
jinshi <jinshikoike@users.noreply.github.com>
2021-10-16 17:24:33 +0900

    backend/tag: Add domain model (#15)
    * backend/tag/* add domain dependency
    
    * backend/tag/domain/src/vo fix import module warning
    
    * backend/tag/domain/src/entity fix import module warning
    
    * add aggregate codes
    
    Co-authored-by: jinshikoike <koikejinshi@google.com>

--- backend/tag/domain/src/vo/mod.rs
+++ backend/tag/domain/src/vo/mod.rs
@@ -1,9 +1,9 @@
 mod fusen_id;
 mod tag_hash;
 mod tag_name;
-mod vo;
+mod value_object;
 
 pub use fusen_id::FusenId;
 pub use tag_hash::TagHash;
 pub use tag_name::TagName;
-pub use vo::ValueObject;
+pub use value_object::ValueObject;
