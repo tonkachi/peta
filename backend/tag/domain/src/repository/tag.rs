5627df5d82e4a0964386277ae07bb3430e469672
jinshi <jinshikoike@users.noreply.github.com>
2021-10-23 14:44:04 +0900

    backend/tag: Change fusend_id field to fusend_ids (#26)
    * change fusen_id field to fusend_ids
    
    * change funsen_id to fusend_ids
    
    * use iterator
    
    Co-authored-by: jinshikoike <koikejinshi@google.com>

--- backend/tag/domain/src/repository/tag.rs
+++ backend/tag/domain/src/repository/tag.rs
@@ -1,13 +1,13 @@
 use crate::aggregate::Tag;
-use crate::vo::TagHash;
+use crate::vo::{FusenId, TagHash};
 use anyhow::{Error, Result};
 
 pub trait TagRepository {
     fn create(&self, entity: Tag) -> Result<(), Error>;
     fn delete(&self, entity: Tag) -> Result<(), Error>;
     fn get(&self, hash: TagHash) -> Result<Tag, Error>;
-    // TODO: fn get_by_fusen_id(&self, fusend_id: FusenId) -> Result<Vec<Tag>, Error>;
-    // TODO: remove fusenid from tag
+    fn get_by_fusen_id(&self, fsend_id: FusenId) -> Result<Vec<Tag>, Error>;
+    fn update_tag(&self, entity: Tag) -> Result<(), Error>;
 }
 
 #[cfg(test)]
@@ -21,14 +21,20 @@ mod tests {
     use std::str::FromStr;
     use std::sync::Mutex;
 
+    macro_rules! from_str {
+        ($t:ty, $s:expr) => {
+            <$t>::from_str($s).unwrap()
+        };
+    }
+
     fn get_dummy_tag(dummy_data: &str) -> Tag {
-        let hash = TagHash::from_str(dummy_data).unwrap();
-        let name = TagName::from_str(dummy_data).unwrap();
-        let fusen_id = FusenId::from_str(dummy_data).unwrap();
+        let hash = from_str!(TagHash, dummy_data);
+        let name = from_str!(TagName, dummy_data);
+        let fusen_id = from_str!(FusenId, dummy_data);
         TagBuilder::default()
             .hash(hash)
             .name(name)
-            .fusen_id(fusen_id)
+            .fusen_ids(vec![fusen_id])
             .build()
             .unwrap()
     }
@@ -49,6 +55,9 @@ mod tests {
     impl TagRepository for TestTagRepository {
         fn create(&self, entity: Tag) -> Result<(), Error> {
             let mut tags = self.tags.lock().unwrap();
+            if tags.get(entity.hash()).is_some() {
+                bail!("tag is already exists")
+            }
             tags.insert(entity.hash().clone(), entity.clone());
             Ok(())
         }
@@ -69,17 +78,36 @@ mod tests {
                 None => bail!("not found tag"),
             }
         }
+
+        fn get_by_fusen_id(&self, fusen_id: FusenId) -> Result<Vec<Tag>, Error> {
+            let tags = self.tags.lock().unwrap();
+            let filtered_tags = tags
+                .values()
+                .filter(|&x| x.fusen_ids().contains(&fusen_id))
+                .map(|x| x.clone())
+                .collect();
+            Ok(filtered_tags)
+        }
+
+        fn update_tag(&self, entity: Tag) -> Result<(), Error> {
+            let mut tags = self.tags.lock().unwrap();
+            if tags.get(entity.hash()).is_none() {
+                bail!("not found tag")
+            }
+            tags.insert(entity.hash().clone(), entity.clone());
+            Ok(())
+        }
     }
 
     #[test]
-    fn test_create_repository() {
+    fn create() {
         let repo = TestTagRepository::new();
         let dummy_tag = get_dummy_tag("dummy");
         assert!(repo.create(dummy_tag).is_ok());
     }
 
     #[test]
-    fn test_delete_repository() {
+    fn delete() {
         let repo = TestTagRepository::new();
         let dummy_tag = get_dummy_tag("dummy");
         repo.create(dummy_tag.clone()).unwrap();
@@ -87,10 +115,73 @@ mod tests {
     }
 
     #[test]
-    fn test_get_repository() {
+    fn get() {
         let repo = TestTagRepository::new();
         let dummy_tag = get_dummy_tag("dummy");
         repo.create(dummy_tag.clone()).unwrap();
         assert!(repo.get(dummy_tag.hash().clone()).is_ok());
+    }
+
+    #[test]
+    fn get_by_fusen_id() {
+        let sut = TestTagRepository::new();
+        let tag1_fusen_id_1_and_2 = TagBuilder::default()
+            .hash(from_str!(TagHash, "tag1"))
+            .name(from_str!(TagName, "tag1"))
+            .fusen_ids(vec![from_str!(FusenId, "f1"), from_str!(FusenId, "f2")])
+            .build()
+            .unwrap();
+        let tag2_fusen_id_1 = TagBuilder::default()
+            .hash(from_str!(TagHash, "tag2"))
+            .name(from_str!(TagName, "tag2"))
+            .fusen_ids(vec![from_str!(FusenId, "f1")])
+            .build()
+            .unwrap();
+        let tag3_fusen_id_3 = TagBuilder::default()
+            .hash(from_str!(TagHash, "tag3"))
+            .name(from_str!(TagName, "tag3"))
+            .fusen_ids(vec![from_str!(FusenId, "f3")])
+            .build()
+            .unwrap();
+
+        sut.create(tag1_fusen_id_1_and_2.clone()).unwrap();
+        sut.create(tag2_fusen_id_1.clone()).unwrap();
+        sut.create(tag3_fusen_id_3.clone()).unwrap();
+
+        let tags_with_f1 = sut.get_by_fusen_id(from_str!(FusenId, "f1")).unwrap();
+        for tag in tags_with_f1 {
+            assert!(vec![tag1_fusen_id_1_and_2.clone(), tag2_fusen_id_1.clone()].contains(&tag));
+        }
+
+        let tags_with_f2 = sut.get_by_fusen_id(from_str!(FusenId, "f2")).unwrap();
+        assert_eq!(tags_with_f2, vec![tag1_fusen_id_1_and_2.clone()])
+    }
+
+    #[test]
+    fn update_tag() {
+        let sut = TestTagRepository::new();
+        let dummy_tag = get_dummy_tag("dummy");
+        sut.create(dummy_tag.clone()).unwrap();
+
+        // get by dummy hash before update
+        let tag_before_update = sut.get(from_str!(TagHash, "dummy")).unwrap();
+        assert_eq!(tag_before_update.name(), dummy_tag.name());
+
+        let new_tag = TagBuilder::default()
+            .hash(from_str!(TagHash, "dummy"))
+            .name(from_str!(TagName, "tag1"))
+            .fusen_ids(vec![from_str!(FusenId, "f1"), from_str!(FusenId, "f2")])
+            .build()
+            .unwrap();
+        sut.update_tag(new_tag.clone()).unwrap();
+
+        let tag_after_update = sut.get(from_str!(TagHash, "dummy")).unwrap();
+        // tag.hash field is used when compare tag entity so in thid test compare tag.name field
+        assert_ne!(tag_after_update.name(), tag_before_update.name());
+        assert_eq!(tag_after_update.name(), new_tag.name());
+
+        // fail when tag is not found
+        let not_found_tag = get_dummy_tag("not_found_tag");
+        assert!(sut.update_tag(not_found_tag.clone()).is_err());
     }
 }
--- backend/tag/usecase/src/interactor/get_tag.rs
+++ backend/tag/usecase/src/interactor/get_tag.rs
@@ -12,10 +12,11 @@ pub struct GetTagInteractor<T: TagRepository> {
 impl<T: TagRepository> Port<GetTagInputData, GetTagOutputData> for GetTagInteractor<T> {
     fn handle(&self, input: GetTagInputData) -> Result<GetTagOutputData, Error> {
         let tag = self.tag_repository.get(input.hash.parse::<TagHash>()?)?;
+        let fusen_ids = tag.fusen_ids().iter().map(|x| x.to_string()).collect();
         Ok(GetTagOutputData {
             hash: tag.hash().clone().to_string(),
             name: tag.name().clone().to_string(),
-            fusen_id: tag.fusen_id().clone().to_string(),
+            fusen_ids,
         })
     }
 }
@@ -33,14 +34,20 @@ mod tests {
     use std::str::FromStr;
     use std::sync::Mutex;
 
+    macro_rules! from_str {
+        ($t:ty, $s:expr) => {
+            <$t>::from_str($s).unwrap()
+        };
+    }
+
     fn get_dummy_tag(dummy_data: &str) -> Tag {
-        let hash = TagHash::from_str(dummy_data).unwrap();
-        let name = TagName::from_str(dummy_data).unwrap();
-        let fusen_id = FusenId::from_str(dummy_data).unwrap();
+        let hash = from_str!(TagHash, dummy_data);
+        let name = from_str!(TagName, dummy_data);
+        let fusen_id = from_str!(FusenId, dummy_data);
         TagBuilder::default()
             .hash(hash)
             .name(name)
-            .fusen_id(fusen_id)
+            .fusen_ids(vec![fusen_id])
             .build()
             .unwrap()
     }
@@ -80,28 +87,47 @@ mod tests {
                 None => bail!("not found tag"),
             }
         }
+
+        fn get_by_fusen_id(&self, fusen_id: FusenId) -> Result<Vec<Tag>, Error> {
+            let tags = self.tags.lock().unwrap();
+            let filtered_tags = tags
+                .values()
+                .filter(|&x| x.fusen_ids().contains(&fusen_id))
+                .map(|x| x.clone())
+                .collect();
+            Ok(filtered_tags)
+        }
+
+        fn update_tag(&self, entity: Tag) -> Result<(), Error> {
+            let mut tags = self.tags.lock().unwrap();
+            if tags.get(entity.hash()).is_none() {
+                bail!("not found tag")
+            }
+            tags.insert(entity.hash().clone(), entity.clone());
+            Ok(())
+        }
     }
 
     #[test]
     fn test_succeeded() {
-        let dummy_tag = get_dummy_tag("dummy_tag");
+        let dummy_tag = get_dummy_tag(&from_str!(String, "dummy_tag"));
         let repository = TestTagRepository::new();
         repository.create(dummy_tag).unwrap();
         let sut = GetTagInteractor::new(repository);
         let input = GetTagInputData {
-            hash: String::from_str("dummy_tag").unwrap(),
+            hash: from_str!(String, "dummy_tag"),
         };
         let output = sut.handle(input).unwrap();
         let expected_output = GetTagOutputData {
-            hash: String::from_str("dummy_tag").unwrap(),
-            name: String::from_str("dummy_tag").unwrap(),
-            fusen_id: String::from_str("dummy_tag").unwrap(),
+            hash: from_str!(String, "dummy_tag"),
+            name: from_str!(String, "dummy_tag"),
+            fusen_ids: vec![from_str!(String, "dummy_tag")],
         };
         assert_eq!(output, expected_output);
         let not_expected_output = GetTagOutputData {
-            hash: String::from_str("fake_dummy_tag").unwrap(),
-            name: String::from_str("fake_dummy_tag").unwrap(),
-            fusen_id: String::from_str("fake_dummy_tag").unwrap(),
+            hash: from_str!(String, "fake_dummy_tag"),
+            name: from_str!(String, "fake_dummy_tag"),
+            fusen_ids: vec![from_str!(String, "fake_dummy_tag")],
         };
         assert_ne!(output, not_expected_output);
     }
